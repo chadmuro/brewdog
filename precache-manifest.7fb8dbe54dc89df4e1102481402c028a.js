545c2c42a86c9845306beaf446c9c0972d24c551
chadmuro <63533886+chadmuro@users.noreply.github.com>
2020-10-24 23:17:02 +0900

    Updates

---
+++ precache-manifest.7fb8dbe54dc89df4e1102481402c028a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73a5e8a3628e2006ef9c73c169d1a20",
+    "url": "/brewdog/index.html"
+  },
+  {
+    "revision": "12358f8994f886801107",
+    "url": "/brewdog/static/css/main.24c6d3b4.chunk.css"
+  },
+  {
+    "revision": "dde592e7eda9698c459f",
+    "url": "/brewdog/static/js/2.6dbb4f9b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/brewdog/static/js/2.6dbb4f9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12358f8994f886801107",
+    "url": "/brewdog/static/js/main.97bf19a4.chunk.js"
+  },
+  {
+    "revision": "c55feef719db27b36856",
+    "url": "/brewdog/static/js/runtime-main.dd306e99.js"
+  },
+  {
+    "revision": "e6e9e33acca60a7d860ae83be96acfdb",
+    "url": "/brewdog/static/media/brewdog.e6e9e33a.jpg"
+  }
+]);
